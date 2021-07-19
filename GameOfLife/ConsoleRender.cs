5868745b0785df628f8388b021a602ad77218f61
darksidecat <58224121+darksidecat@users.noreply.github.com>
2021-07-19 20:30:26 +0300

    separate console render and logic

---
+++ GameOfLife/ConsoleRender.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    class GameOfLife
+    {
+        static void Main(string[] args)
+        {
+            Config config = new(0.15f);
+            ParseArgs(args, ref config);
+
+            Field field = new(config.Height, config.Width);
+            field.FillRandom(config.FillPercent);
+            Render(field, config);
+        }
+        static void ParseArgs(string[] args, ref Config config)
+        {
+            if (args.Length >= 1 && float.TryParse(args[0], out _))
+            {
+                config.FillPercent = float.Parse(args[0]);
+            }
+            if (args.Length >= 2 && int.TryParse(args[1], out _))
+            {
+                config.UpdateTime = int.Parse(args[1]);
+            }
+            if (args.Length >= 3 && int.TryParse(args[2], out _))
+            {
+                config.Height = int.Parse(args[2]);
+            }
+            if (args.Length >= 4 && int.TryParse(args[3], out _))
+            {
+                config.Width = int.Parse(args[3]);
+            }
+        }
+        public static string Frame(byte[,] field)
+        {
+            string frame = string.Empty;
+
+            for (int i = 0; i < field.GetLength(0); i++)
+            {
+                for (int j = 0; j < field.GetLength(1); j++)
+                {
+                    frame += field[i, j] == 1 ? ((char)Cell.ALIVE) : ((char)Cell.DEAD);
+                }
+                frame += '\n';
+            }
+
+            return frame;
+        }
+        public static void ReadInput(ref Field field, ref Config config)
+        {
+            if (Console.KeyAvailable)
+            {
+                ConsoleKeyInfo ski = Console.ReadKey(true);
+                switch (ski.Key)
+                {
+                    case ConsoleKey.R:
+                        field.FillRandom(config.FillPercent);
+                        break;
+                    case ConsoleKey.X:
+                        config.UpdateTime += 100;
+                        break;
+                    case ConsoleKey.Z:
+                        config.UpdateTime -= 100;
+                        break;
+                    case ConsoleKey.C:
+                        config.UpdateTime = 20;
+                        break;
+                }
+            }
+        }
+
+        public static void Render(Field field, Config config)
+        {
+            float frameTime = 0;
+            int frameCounter = 0;
+            DateTime old = DateTime.Now;
+            Console.Title = "GameOfLife";
+
+            while (true)
+            {
+                Console.CursorVisible = false;
+
+                frameCounter += 1;
+                if (frameCounter % 10 == 0)
+                {
+                    DateTime now = DateTime.Now;
+                    frameTime = (float)now.Subtract(old).TotalSeconds / 10.0f;
+                    old = now;
+                }
+
+                ReadInput(ref field, ref config);
+
+                if (config.UpdateTime != 1) System.Threading.Thread.Sleep(config.UpdateTime);
+                Console.SetCursorPosition(0, 0);
+                Console.Write(Frame(field.FieldCurrent));
+                Console.Write($"\nR - refresh, Z - faster, X - slower, C - comfort\n" +
+                    $"Fill: {config.FillPercent}%     \nSleep time:{config.UpdateTime}     \nFramerate: {1.0f / frameTime}     ");
+                field.UpdateField();
+            }
+        }
+
+    }
+
+    enum Cell
+    {
+        ALIVE = '■',
+        DEAD = ' '
+    }
+
+
+}
--- GameOfLife/GameOfLife.cs
+++ GameOfLife/GameOfLife.cs
@@ -3,39 +3,6 @@
 
 namespace GameOfLife
 {
-    class GameOfLife
-    {
-        static void Main(string[] args)
-        {
-            Config config = new(0.15f);
-            ParseArgs(args, ref config);
-
-            Field field = new(config.Height, config.Width);
-            field.FillRandom(config.FillPercent);
-            field.Render(field, config);
-        }
-        static void ParseArgs(string[] args, ref Config config)
-        {
-            if (args.Length >= 1 && float.TryParse(args[0], out _))
-            {
-                config.FillPercent = float.Parse(args[0]);
-            }
-            if (args.Length >= 2 && int.TryParse(args[1], out _))
-            {
-                config.UpdateTime = int.Parse(args[1]);
-            }
-            if (args.Length >= 3 && int.TryParse(args[2], out _))
-            {
-                config.Height = int.Parse(args[2]);
-            }
-            if (args.Length >= 4 && int.TryParse(args[3], out _))
-            {
-                config.Width = int.Parse(args[3]);
-            }
-        }
-
-    }
-
     public ref struct Config
     {
         private float _fillPercent;
@@ -100,21 +67,19 @@ namespace GameOfLife
     }
 
 
-    enum Cell
-    {
-        ALIVE = '#',
-        DEAD = ' '
-    }
-
-
     class Field
     {
         private readonly byte Alive = 1;
         private readonly byte Dead = 0;
         private readonly int Height;
         private readonly int Width;
-        private byte[,] Field_now;
-        private byte[,] Field_next;
+        private byte[,] FieldNow;
+        private byte[,] FieldNext;
+
+        public byte[,] FieldCurrent
+        {
+            get => FieldNow;
+        }
 
         public Field() : this(10, 10)
         {
@@ -123,8 +88,8 @@ namespace GameOfLife
         {
             Height = height;
             Width = width;
-            Field_now = new byte[height, width];
-            Field_next = new byte[height, width];
+            FieldNow = new byte[height, width];
+            FieldNext = new byte[height, width];
         }
 
         public byte[,] FillRandom(float fillPercent)
@@ -135,10 +100,10 @@ namespace GameOfLife
             {
                 for (int j = 0; j < Width; j++)
                 {
-                    Field_now[i, j] = rnd.NextDouble() <= fillPercent ? Alive : Dead;
+                    FieldNow[i, j] = rnd.NextDouble() <= fillPercent ? Alive : Dead;
                 }
             }
-            return Field_now;
+            return FieldNow;
         }
 
         public void UpdateField()
@@ -147,23 +112,23 @@ namespace GameOfLife
             {
                 for (int j = 1; j < Width - 1; j++)
                 {
-                    bool isAlive = Field_now[i, j] == 1;
+                    bool isAlive = FieldNow[i, j] == 1;
 
                     byte numNeigbours =
                         (byte)
-                        (Field_now[i - 1, j] +
-                        Field_now[i - 1, j - 1] +
-                        Field_now[i - 1, j + 1] +
-                        Field_now[i, j - 1] +
-                        Field_now[i + 1, j] +
-                        Field_now[i + 1, j + 1] +
-                        Field_now[i + 1, j - 1] +
-                        Field_now[i, j + 1]);
+                        (FieldNow[i - 1, j] +
+                        FieldNow[i - 1, j - 1] +
+                        FieldNow[i - 1, j + 1] +
+                        FieldNow[i, j - 1] +
+                        FieldNow[i + 1, j] +
+                        FieldNow[i + 1, j + 1] +
+                        FieldNow[i + 1, j - 1] +
+                        FieldNow[i, j + 1]);
 
                     bool stayAlive = isAlive && (numNeigbours == 2 || numNeigbours == 3);
                     bool born = !isAlive && numNeigbours == 3;
 
-                    Field_next[i, j] = stayAlive | born ? Alive : Dead;
+                    FieldNext[i, j] = stayAlive | born ? Alive : Dead;
 
                 }
             }
@@ -171,79 +136,11 @@ namespace GameOfLife
             {
                 for (int j = 0; j < Width; j++)
                 {
-                    Field_now[i, j] = Field_next[i, j];
+                    FieldNow[i, j] = FieldNext[i, j];
                 }
-            }
-        }
-
-        public static string Frame(byte[,] field)
-        {
-            string frame = string.Empty;
-
-            for (int i = 0; i < field.GetLength(0); i++)
-            {
-                for (int j = 0; j < field.GetLength(1); j++)
-                {
-                    frame += field[i, j] == 1 ? ((char)Cell.ALIVE) : ((char)Cell.DEAD);
-                }
-                frame += '\n';
-            }
-
-            return frame;
-        }
-        public static void ReadInput(ref Field field, ref Config config)
-        {
-            if (Console.KeyAvailable)
-            {
-                ConsoleKeyInfo ski = Console.ReadKey(true);
-                switch (ski.Key)
-                {
-                    case ConsoleKey.R:
-                        field.FillRandom(config.FillPercent);
-                        break;
-                    case ConsoleKey.X:
-                        config.UpdateTime += 100;
-                        break;
-                    case ConsoleKey.Z:
-                        config.UpdateTime -= 100;
-                        break;
-                    case ConsoleKey.C:
-                        config.UpdateTime = 17;
-                        break;
-                }
-            }
-        }
-
-        public void Render(Field field, Config config)
-        {
-            float frameTime = 0;
-            int frameCounter = 0;
-            DateTime old = DateTime.Now;
-
-            
-
-            while (true)
-            {
-                frameCounter += 1;
-                if (frameCounter % 10 == 0)
-                {
-                    DateTime now = DateTime.Now;
-                    frameTime = (float)now.Subtract(old).TotalSeconds / 10.0f;
-                    old = now;
-                }
-
-                ReadInput(ref field, ref config);
-
-                if (config.UpdateTime != 1) System.Threading.Thread.Sleep(config.UpdateTime);
-                Console.SetCursorPosition(0, 0);
-                Console.Write(Frame(Field_now));
-                Console.Write($"\nR - refresh, Z - faster, X - slower, C - comfort\n" +
-                    $"Fill: {config.FillPercent}%     \nSleep time:{config.UpdateTime}     \nFramerate: {1.0f / frameTime}     ");
-                UpdateField();
             }
         }
 
     }
-
 
 }
